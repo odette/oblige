af420134ae50bdc4a3ec14b5da6993b28f0a256a
y_sugasawa <bended.string.master@gmail.com>
2012-07-01 10:44:19 +0900

    First-commit
    Signed-off-by: y_sugasawa <bended.string.master@gmail.com>

---
+++ src/jp/ddo/trismegistos/oblige/BaseApplication.java
@@ -0,0 +1,26 @@
+package jp.ddo.trismegistos.oblige;
+
+import jp.ddo.trismegistos.oblige.holder.ApplicationHolder;
+import android.app.Application;
+import android.util.Log;
+
+/**
+ * Applicationの基底クラス。
+ * 
+ * @author y_sugasawa
+ * @since 2012/07/01
+ */
+public class BaseApplication extends Application {
+
+	/** タグ */
+	private static final String TAG = "BAseAppication";
+
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	public void onCreate() {
+		Log.d(TAG, "onCreate()");
+		ApplicationHolder.set(this);
+	}
+}
